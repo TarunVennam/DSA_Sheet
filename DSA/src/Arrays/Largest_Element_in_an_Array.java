8754619176db91183ee22e9d4e886017de88752b
Tarun <vennamtarun123@gmail.com>
2024-08-06 12:55:55 +0530

    Arrays

---
+++ DSA/src/Arrays/Largest_Element_in_an_Array.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class Largest_Element_in_an_Array {
+
+    public static void main(String[] args) {
+
+    }
+    static int largest(int n, int[] arr) {
+        // code here
+        int max = Integer.MIN_VALUE;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
